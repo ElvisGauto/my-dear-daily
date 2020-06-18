03868ca01eb2a91deb4ce6d48a34e33542f0b294
Mauro Gauto <elvis.gauto1999@gmail.com>
2020-06-17 21:33:46 -0300

    add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './core/components/login/login.component';
+import { HomeComponent } from './core/components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
 @NgModule({
-  declarations: [ComponentsComponent],
+  declarations: [
+    HomeComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+
   ]
 })
 export class CoreModule { }
